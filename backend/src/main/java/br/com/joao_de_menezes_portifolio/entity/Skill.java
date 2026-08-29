859eb54642edb67d8fb5c5c54fd009b2fe250127
Joao de Menezes <juniormenezes384@gnail.com>
2026-08-29 17:57:29 -0300

    fix: using @Getter and @Setter from lombok

--- backend/src/main/java/br/com/joao_de_menezes_portifolio/entity/Skill.java
+++ backend/src/main/java/br/com/joao_de_menezes_portifolio/entity/Skill.java
@@ -4,7 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 public class Skill {
 
@@ -19,35 +23,4 @@ public class Skill {
     public Skill() {
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getLevel() {
-        return level;
-    }
-
-    public void setLevel(String level) {
-        this.level = level;
-    }
 }
